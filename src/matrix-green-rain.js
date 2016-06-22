a4f2ba956c1b64861702b426fafc61f084bf0169
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-22 12:24:48 -0400

    first steps to use react to support multiple sketches

--- src/matrix-green-rain.js
+++ src/matrix-green-rain.js
@@ -2,7 +2,7 @@
 
 import p5 from 'p5'
 import { unfold } from 'ramda'
-
+import React from 'react'
 
 // custom classes actually implement as plain old JS objects
 class Symbol {
@@ -78,4 +78,17 @@ const sketch = p => {
   };
 }
 
-new p5(sketch)
+export default class MatrixGreenRain extends React.Component {
+  constructor() {
+    super();
+    this.id = "matrix-green-rain";
+  }
+
+  render() {
+    return <div id={this.id} />;
+  }
+
+  componentDidMount() {
+    new p5(sketch)    
+  }
+}

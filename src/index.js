21e804f7907801b36db66586bc088710fe0d9bb5
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-27 15:23:36 -0400

    mostly working with ugly hash history

--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Router, Route, browserHistory } from 'react-router'
+import { Router, Route, hashHistory } from 'react-router'
+import { createHashHistory, useBasename } from 'history'
 
 import Sidebar from './sidebar'
 import MatrixGreenRain from './matrix-green-rain'
 import BoxingTimer from './boxing-timer'
 
 
+const basename = '/p5-sketches',
+      myHistory = useBasename(createHashHistory)({basename: basename});
+
 ReactDOM.render(
   (
-  <Router history={browserHistory}>
-    <Route path="/" component={Sidebar}/>
-    <Route path="/matrix-green-rain" component={MatrixGreenRain}/>
-    <Route path="/boxing-timer" component={BoxingTimer}/>
+  <Router history={myHistory}>
+    <Route path='/' component={Sidebar}/>
+    <Route path={basename} component={Sidebar}/>
+    <Route path='matrix-green-rain' component={MatrixGreenRain}/>
+    <Route path='boxing-timer' component={BoxingTimer}/>
   </Router>
   ),
   document.getElementById("root"))

53292a33c627ff1bb551f59fa87356488f783d53
Nabil Hassein <nabil.hassein@gmail.com>
2016-06-28 17:33:36 -0400

    simple non-interactive visual only boxing timer

--- src/boxing-timer.js
+++ src/boxing-timer.js
@@ -1,14 +1,32 @@
 import React from 'react'
+import CountdownClock from './countdown-clock'
+
+// GLOBAL STATE
+let active = 0;
+
+const states = [
+  { color: "green" , seconds: 150 },
+  { color: "yellow", seconds: 30 },
+  { color: "red"   , seconds: 60 },
+];
+
 
 export default class BoxingTimer extends React.Component {
+  clockDone = () => {
+    active = (active + 1) % 3;
+    this.setState(states[active]);
+  }
+
   constructor() {
     super();
     this.id = "boxing-timer";
+    this.state = states[0];
   }
 
   render() {
+    const size = 0.8 * Math.min(screen.availWidth, screen.availHeight);
     return (<div id={this.id}>
-            "TODO"
-             </div>) ;
+            <CountdownClock size={size} seconds={this.state.seconds} color={this.state.color} onComplete={this.clockDone} />
+            </div>);
   }
 }
